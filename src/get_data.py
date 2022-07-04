c4c293b5aa1e1e3b8a02b5a72bee3ac66561bd8c
prabhat-123 <srv.ale52@gmail.com>
2022-07-04 17:27:53 +0545

    stage 1 complete(data loading and saving)

--- src/get_data.py
+++ src/get_data.py
@@ -16,7 +16,6 @@ def read_params(config_path):
 
 def get_data(config_path):
     config = read_params(config_path)
-    print(config)
     data_path = config["data_source"]["s3_source"]
     df = pd.read_csv(data_path, sep=",", encoding="utf-8")
     return df
