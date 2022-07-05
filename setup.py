6ab3f7111fedabd1d3cbf36099f84b42f7cdd09e
prabhat-123 <srv.ale52@gmail.com>
2022-07-06 00:15:47 +0545

    setup completed

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="It is a wine quality prediction project",
+    author="prabhat",
+    packages=find_packages(),
+    license="MIT",
+    license_file="LICENSE"
+)
